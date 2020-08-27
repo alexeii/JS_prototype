729f5ca7576154a755fd77541187aa5a6ed45411
Alexey Arhipiv <alex3171@list.ru>
2020-08-27 14:08:24 +0500

    fix:Home work

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 "use strict";
-
-//window.addEventListener("DOMContentLoaded", function () {
+//document.addEventListener("DOMContentLoaded", domElement.newDiv);
 const DomElement = function (selector, height, width, bg, fontSize) {
   this.selector = selector;
   this.height = height;
@@ -10,20 +9,32 @@ const DomElement = function (selector, height, width, bg, fontSize) {
 };
 DomElement.prototype.newDiv = function () {
   let newDiv = document.createElement("div");
+  let name = this.selector.slice(1);
   if (this.selector[0] === ".") {
-    newDiv.className = this.selector;
+    newDiv.className = name;
   } else if (this.selector[0] === "#") {
-    newDiv.id = this.selector;
+    newDiv.id = name;
   }
   document.body.prepend(newDiv);
-  newDiv.textContent = prompt("Введите текст нового блока", "Привет");
+  //newDiv.textContent = prompt("Введите текст нового блока", "Привет");
   newDiv.style.cssText = `height: ${this.height}; 
             width: ${this.width}; 
             background-color: ${this.backgroundColor}; 
-            font-size: ${this.fontSize}`;
-};
+            font-size: ${this.fontSize};
+            position: absolute`;
 
+  document.addEventListener("keydown", function (event) {
+    console.log(event);
+    switch (event.code) {
+      case "ArrowRight":
+        newDiv.style.left += 10 + "px";
+        break;
+      case "ArrowLeft":
+        newDiv.style.right += 10 + "px";
+      //break;
+    }
+  });
+};
 const domElement = new DomElement("#block", "100px", "100px", "red", "12px");
-//window.addEventListener("DOMContentLoaded", domElement.makeNewDiv, false);
+
 domElement.newDiv();
-//});
